d11a03debc5f07430f58f19b241ff4f0ccdfec26
darshan-bane <darshanbane253@gmail.com>
2024-03-22 18:00:19 +0530

    done bottle moving left to right

--- script.js
+++ script.js
@@ -8,10 +8,55 @@ var tl = gsap.timeline({scrollTrigger:{
 
 tl.to("#fanta",{
     top: "120%",
-    left: "0%"
+    left: "0%",
+    // zIndex:"5"
 }, 'orange')
 tl.to("#leaf",{
     top: "120%",
-    left: "200%"
+    left: "100%",
+    rotate: "0deg"
+}, 'orange')
+tl.to("#fanta_head",{
+    zIndex:"0"
 }, 'orange')
 
+
+var tl = gsap.timeline({scrollTrigger:{
+    trigger: ".sectwo",
+    start: "0% 95%",
+    end: "70% 50%",
+    scrub: true,
+    // markers: true,
+}})
+
+tl.to("#fanta",{
+    top: "120%",
+    left: "0%",
+    // zIndex:"5"
+}, 'orange')
+tl.to("#leaf",{
+    top: "120%",
+    left: "100%",
+    rotate: "0deg"
+}, 'orange')
+tl.to("#fanta_head",{
+    zIndex:"0"
+}, 'orange')
+
+var tl2 = gsap.timeline({scrollTrigger:{
+    trigger: ".sec3",
+    start: "0% 95%",
+    end: "70% 50%",
+    scrub: true,
+    // markers: true,
+}})
+tl2.to("#fanta",{
+    top: "220%",
+    left: "58%",
+    // zIndex:"5"
+}, 'ca')
+tl2.to("#leaf",{
+    top: "269%",
+    left: "30%",
+    rotate: "50deg"
+}, 'ca')
